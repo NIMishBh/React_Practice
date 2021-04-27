1f1d4dafe997140c1f590cb0b762d1d7f058c32c
nimishh <nimish864@gmail.com>
2021-04-28 02:17:04 +0530

    Made a Promise and executed Async Read & Write

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const EncodeAsyncRead = require('./src/EncodeAsyncRead')
 const EncodeAsyncWrite = require('./src/EncodeAsyncWrite')
 
 EncodeAsyncRead("input.txt").then(data => {
-  EncodeAsyncWrite("out.txt", data).then(data => {
+  EncodeAsyncWrite("out.txt", data).then(
     console.log("Write successful in the Output File")
-  })
-  .catch(err =>{
-    console.log(err)
+  )
+  .catch(err => {
+    console.log("Error in Writing : " + err)
   })
 })
 .catch(err => {
-  console.error(err);
+  console.error("Error in Reading : " + err);
 })
